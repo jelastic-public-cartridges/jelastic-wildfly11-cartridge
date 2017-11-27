add28f94ec3dfad8fce9df1eb23605719fe07a00
Dmytro Zubelevych <dz@jelastic.com>
2017-11-27 10:54:50 +0200

    remove outdated redirects

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -eu
 
-/sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990;
-/sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993;
 /sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
